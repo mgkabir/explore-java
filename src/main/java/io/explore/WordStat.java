89a061c1f25c0c5a691e0ee427d2b8706a44f276
Mohammad Kabir <mgkabir@gmail.com>
2018-02-12 07:11:00 +1100

    wordstat

--- src/main/java/io/explore/WordStat.java
+++ src/main/java/io/explore/WordStat.java
@@ -1,11 +1,11 @@
 package io.explore;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.text.NumberFormat;
+import java.util.Arrays;
 
 public class WordStat {
 
@@ -13,13 +13,23 @@ public class WordStat {
 
 		String file = "src/main/resources/words.txt";
 
-		Stream<String> stream = Files.lines(Paths.get(file));
-
-		List<String> list =  stream.filter(line -> line.startsWith("Th")).map(String::toUpperCase).collect(Collectors.toList());
-
-		stream.close();
-		list.forEach(System.out::println);
-
+		long totalWordCount = Files.lines(Paths.get(file)).flatMap(line -> Arrays.stream(line.split(" "))).count();
+		System.out.println("Total Words : " + totalWordCount);
+		long distinctWordCount = Files.lines(Paths.get(file)).flatMap(line -> Arrays.stream(line.split(" "))).distinct()
+				.count();
+		System.out.println("Distinct Words : " + distinctWordCount);
+		
+		long largeWordCount = Files.lines(Paths.get(file)).flatMap(line -> Arrays.stream(line.split(" ")))
+				.filter(w -> w.length() >= 7).count();
+		System.out.println("Words with >=7 letters : " + largeWordCount);
+		
+		long smallWordCount = Files.lines(Paths.get(file)).flatMap(line -> Arrays.stream(line.split(" ")))
+				.filter(w -> w.length() < 7).count();
+		System.out.println("Words with < 7 letters : " + smallWordCount);
+		
+		double avgLength = Files.lines(Paths.get(file)).flatMap(line -> Arrays.stream(line.split(" "))).mapToLong(w->w.length()).average().orElse(0);
+				
+		System.out.println("Average Word Count : " + NumberFormat.getInstance().format(avgLength));
 	}
 
 }
