9afd7fe96f3d52b3f4f2511de7a75ef7e2040533
Mohammad Kabir <mkabir@semanticsoftware.com.au>
2018-05-01 11:35:11 +1000

    Akka exploration starts

---
+++ src/main/java/io/explore/akka/Main.java
@@ -0,0 +1,52 @@
+package io.explore.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class Main {
+
+	static class Counter extends AbstractActor {
+
+		private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+		// protocol
+		static class Message {
+		}
+
+		private int counter = 0;
+
+		@Override
+		public Receive createReceive() {
+			return receiveBuilder().match(Message.class, this::onMessage).build();
+		}
+
+		private void onMessage(Message m) {
+			counter++;
+			log.info("Counter : " + counter);
+		}
+
+		public static Props props() {
+			return Props.create(Counter.class);
+		}
+	}
+
+	public static void main(String[] args) {
+		ActorSystem system = ActorSystem.create();
+		ActorRef counter = system.actorOf(Counter.props(), "counter");
+
+		for (int i = 0; i < 5; i++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					for (int j = 0; j < 5; j++) {
+						counter.tell(new Counter.Message(), ActorRef.noSender());
+					}
+				}
+			}).start();
+		}
+	}
+}
