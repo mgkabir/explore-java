1813b8871117d269110adaca92442a715435a1b8
Mohammad Kabir <mgkabir@gmail.com>
2018-11-29 15:26:24 +1100

    python script reads args sent from Java

--- src/main/java/io/explore/python/JavaPython.java
+++ src/main/java/io/explore/python/JavaPython.java
@@ -8,17 +8,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class JavaPython {
-
+    private static String pyScriptShowNum = "src/main/resources/numbers.py";
+    private static String pyScriptAddNum = "src/main/resources/add_numbers.py";
     public static void main(String args[]) throws Exception {
         long start = System.currentTimeMillis();
-        printToConsole(runPythonInProcess());
-        writeResultToFile(runPythonInProcess());
+        printToConsole(runPythonInProcess(pyScriptShowNum));
+        writeResultToFile(runPythonInProcess(pyScriptShowNum),"displayNum.txt");
+        printToConsole(runPythonInProcess(pyScriptAddNum, "7", "9"));
+        writeResultToFile(runPythonInProcess(pyScriptAddNum,"7","9"),"addNum.txt");
         System.out.println(String.format("Time taken : %s ms", System.currentTimeMillis() - start));
     }
 
-    private static BufferedReader runPythonInProcess() throws Exception {
+    private static BufferedReader runPythonInProcess(String pySource, String... param) throws Exception {
 
-        ProcessBuilder pb = new ProcessBuilder("python","src/main/resources/numbers.py");
+        ProcessBuilder pb = new ProcessBuilder();
+        if(param.length != 2){
+            pb.command("python",pySource);
+        }else{
+            pb.command("python",pySource,param[0],param[1]);
+        }
+
         Process p = pb.start();
         BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
         return br;
@@ -30,13 +39,14 @@ public class JavaPython {
         });
     }
 
-    private static void writeResultToFile(BufferedReader br) throws IOException {
+    private static void writeResultToFile(BufferedReader br, String fileName) throws IOException {
         StringBuffer strBuff = new StringBuffer();
+
         br.lines().forEach(line -> {
             strBuff.append(line).append(" ");
         });
 
-        Path path = Paths.get("result.txt");
+        Path path = Paths.get(fileName);
         Files.write(path, strBuff.toString().getBytes());
     }
 }
---
+++ src/main/resources/add_numbers.py
@@ -0,0 +1,4 @@
+import sys
+print ("Sum of 2 given number : ")
+
+print (int(sys.argv[1])+ int(sys.argv[2]))
